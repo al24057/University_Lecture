e14925a3d0e9c4e8fd1828b6123379ca7cdc3212
al24057 <yushitennis3@icloud.com>
2025-12-16 16:12:15 +0900

    Create regular expression

--- Lecture_Search/models.py
+++ Lecture_Search/models.py
@@ -14,7 +14,7 @@ class Lecture(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     instructor = models.CharField(max_length=100, verbose_name="講師名")
     year = models.IntegerField()
-    day_of_week = models.IntegerField(verbose_name="曜日", choices=[(0,"月曜"),(1,"火曜"),(2,"水曜"),(3,"木曜"),(4,"金曜"),(5,"土曜"),(6,"日曜")])
+    day_of_week = models.IntegerField(verbose_name="曜日", choices=[(0,"月曜"),(1,"火曜"),(2,"水曜"),(3,"木曜"),(4,"金曜"),(5,"土曜")])
     period = models.IntegerField()
     description = models.TextField(max_length=1000)
     
--- Lecture_Search/views.py
+++ Lecture_Search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 import MeCab
+import re
 # Create your views here.
 
 class IndexView(View):
@@ -8,8 +9,19 @@ class IndexView(View):
         return render(request, "Lecture_Search/index.html")
     def post(self,request):
         prompt = request.POST.get("prompt")
+        
+        KANJI_NUM = {"一":"1", "二":"2", "三":"3", "四":"4", "五":"5", "六":"6",}
+        
+        for k,v in KANJI_NUM:
+            prompt = prompt.replace(k,v)
+        
+        match = re.findall(r"(月|火|水|木|金|土)(?:曜|曜日)?\s*(?:の)?\s*(\d+(?:\s*[、,]\s*\d+)*)(?:時)?限", prompt)
+        days_match = re.findall(r"(月|火|水|木|金|土)(?:曜|曜日)?", prompt)
+        periods_match = re.findall(r"(\d|一|二|三|四|五|六)(?:時)?限", prompt)
+
+        
         t=MeCab.Taggar()
         result = t.parse(prompt)
-        曜日マップ = {"月曜":0, "火曜":1, "水曜":2, "木曜":3, "金曜":4, "土曜":5, "日曜":6}
+        曜日マップ = {"月曜":0, "火曜":1, "水曜":2, "木曜":3, "金曜":4, "土曜":5}
     
 index = IndexView.as_view()
